8d12ead05a95f75547db5b23b31bf2bb8d85b588
liye0717 <liubou.yerameyeva@netcracker.com>
2019-06-20 17:40:06 +0300

    L2: app view with stub data

--- src/app/cars-form/cars-form.component.ts
+++ src/app/cars-form/cars-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Car} from '../car.model';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-cars-form',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarsFormComponent implements OnInit {
 
-  constructor() { }
+  private id = 2;
+
+  carName = '';
+  carModel = '';
+
+  @Output() addCar = new EventEmitter<Car>();
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onAdd() {
+    if (this.carName === '' || this.carModel === '') {
+      return;
+    }
+
+    this.id = ++this.id;
+
+    const car = new Car(
+      this.carName,
+      moment().format('DD.MM.YY'),
+      this.carModel,
+      false,
+      this.id
+    );
+
+    this.addCar.emit(car);
+
+    this.carName = '';
+    this.carModel = '';
+  }
+
+  onLoad() {
+    // todo
+  }
 }
