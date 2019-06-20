985d61f9ac3c4a7f83ca5200313569d1f4253208
liye0717 <liubou.yerameyeva@netcracker.com>
2019-06-20 18:10:15 +0300

    L7: Getting data from the store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import {Component} from '@angular/core';
-import {Car} from './car.model';
+import {Component, OnInit} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {Car, Cars} from './car.model';
+import {AppState} from './redux/app.state';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   public cars: Car[] = [];
+  public carState: Observable<Cars>;
+
+  constructor(private store: Store<AppState>) {
+  }
+
+  ngOnInit(): void {
+    // this.store.select('carPage').subscribe(({cars}) => {
+    //   this.cars = cars;
+    // });
+    this.carState = this.store.select('carPage');
+  }
 
   onAdd(car: Car) {
     this.cars.push(car);
