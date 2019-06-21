d71112f958007f0b08dd0bbc5a712173c84fd377
liye0717 <liubou.yerameyeva@netcracker.com>
2019-06-21 16:57:26 +0300

    L16: Routing & dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+
 import {EffectsModule} from '@ngrx/effects';
 import {StoreModule} from '@ngrx/store';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 
 import {AppComponent} from './app.component';
 import {CarComponent} from './car/car.component';
+import {CarsService} from './cars.service';
 import {CarsFormComponent} from './cars-form/cars-form.component';
 import {CarsEffect} from './redux/cars.effect';
 import {carsReducer} from './redux/cars.reducer';
-import {CarsService} from './cars.service';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -23,7 +28,14 @@ import {CarsService} from './cars.service';
     FormsModule,
     HttpClientModule,
     EffectsModule.forRoot([CarsEffect]),
-    StoreModule.forRoot({carPage: carsReducer})
+    StoreModule.forRoot({carPage: carsReducer}),
+    RouterModule.forRoot([
+      {path: '', component: AppComponent}
+    ]),
+    StoreRouterConnectingModule.forRoot(),
+    StoreDevtoolsModule.instrument({
+      logOnly: environment.production, // Restrict extension to log-only mode
+    })
   ],
   providers: [
     CarsService
